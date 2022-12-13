571f0a2e48ef287865af74336fdfc70776748aa4
Dre <bryodiiidah@gmail.com>
2022-12-13 14:02:49 +0000

    add component update actions

--- api-template/components/ComponentAccordion.tsx
+++ api-template/components/ComponentAccordion.tsx
@@ -4,18 +4,28 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Button, FormControl, InputLabel, MenuItem } from "@mui/material";
+import {
+  AccordionActions,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
 import Select from "./util/components/Select";
 import { usePagesStateValue } from "../lib/builder";
 
 export default function CompontentsAccordion({ component, index }) {
-  const pages = usePagesStateValue("pages");
   const [componentData, setComponentData] = React.useState(() => component);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setComponentData((p) => ({ ...p, [name]: value }));
   };
+
+  const { handleChange: update, handleDelete: deleteComponent } = useActions();
+
+  const handleUpdate = () => update(componentData, index);
+  const handleDelete = () => deleteComponent(index);
 
   return (
     <div>
@@ -36,6 +46,7 @@ export default function CompontentsAccordion({ component, index }) {
               id="demo-simple-select"
               value={componentData?.type}
               label="Type"
+              name="type"
               onChange={handleChange}
             >
               <MenuItem value={"box"}>Box</MenuItem>
@@ -43,9 +54,58 @@ export default function CompontentsAccordion({ component, index }) {
               <MenuItem value={"button"}>Button</MenuItem>
             </Select>
           </FormControl>
-          <Button sx={{ mt: 3 }}>Update</Button>
+          <AccordionActions>
+            <Button
+              variant="outlined"
+              disabled={component.type === componentData.type}
+              sx={{ mt: 3 }}
+              onClick={handleUpdate}
+            >
+              Update
+            </Button>
+            <Button
+              variant="outlined"
+              color="error"
+              sx={{ mt: 3 }}
+              onClick={handleDelete}
+            >
+              Delete{" "}
+            </Button>
+          </AccordionActions>
         </AccordionDetails>
       </Accordion>
     </div>
   );
+}
+
+function useActions() {
+  const dispatch = usePagesStateValue("dispatch");
+  const pages = usePagesStateValue("pages");
+  const pageIndex = usePagesStateValue("pageIndex");
+  return {
+    handleChange(component, index) {
+      try {
+        let allPages = [...pages];
+        let page = allPages[pageIndex];
+        page.components[index] = component;
+        allPages[pageIndex] = page;
+        const type = "update_all";
+        const payload = allPages;
+        const key = "pages";
+        dispatch({ type, key, payload });
+      } catch (e) {}
+    },
+    handleDelete(index) {
+      try {
+        let allPages = [...pages];
+        let page = allPages[pageIndex];
+        page.components.splice(index, 1);
+        allPages[pageIndex] = page;
+        const type = "update_all";
+        const payload = allPages;
+        const key = "pages";
+        dispatch({ type, key, payload });
+      } catch (e) {}
+    },
+  };
 }
